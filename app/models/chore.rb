d8e4a35e4dddedf4d485cea8c99f873735d471ac
aiyana <xinyih@andrew.cmu.edu>
2021-02-03 22:32:18 -0800

    Added to Chore model to pass Chore tests

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,23 @@
 class Chore < ApplicationRecord
     belongs_to :child
     belongs_to :task
+
+    validates_date :due_on
+
+    scope :by_task, -> { joins(:task).order('name')}
+    scope :chronological, -> {order('due_on, completed')}
+    scope :pending, -> { where(completed: false) }
+    scope :done, -> { where(completed: true) }
+    scope :past, -> { where("due_on < ? ", Date.current) }
+    scope :upcoming, -> { where("due_on >= ? ", Date.current) }
+    
+    def status
+        if completed
+            "Completed"
+        else
+            "Pending"
+        end
+    end
+
+
 end
